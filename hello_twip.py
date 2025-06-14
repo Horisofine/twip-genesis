cb8cb97ea07ba65615b3e564421a37e5c506ae5f
Houssam E. Righi <hrighi01@gmail.com>
2025-06-13 21:00:57 -0400

    Hello twip

--- hello_twip.py
+++ hello_twip.py
@@ -1,18 +1,43 @@
 import genesis as gs
+import numpy as np
 
-gs.init(backend=gs.metal)
+gs.init(backend=gs.cuda)
 
-scene = gs.Scene()
+scene = gs.Scene(
+    show_viewer=True,
+)
 
 plane = scene.add_entity(
     gs.morphs.Plane(),
 )
-franka = scene.add_entity(
+twip = scene.add_entity(
     gs.morphs.URDF(
-        file="assets/twip.urdf"
+        file="assets/twip.urdf",
+        pos=[0, 0, 0.1]
     )
 )
 
+joints_name = [
+    "rwheel",
+    "lwheel",
+]
+
+motors_dof_idx = [twip.get_joint(name).dof_idx_local for name in joints_name]
+
 scene.build()
+flip = True
+
 for i in range(1000):
+    if i % 50 == 0:
+        flip = not flip
+
+    if flip:
+        velocity = np.array([10.0, 10.0])
+    else:
+        velocity = np.array([-10.0, -10.0])
+
+    twip.control_dofs_velocity(
+        velocity,
+        motors_dof_idx,
+    )
     scene.step()
