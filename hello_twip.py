06e85c02250f1ed42a4d4ca78128697a3c704582
Houssam E. Righi <hrighi01@gmail.com>
2025-06-14 12:15:25 -0400

    Got somehat of a functional model

--- hello_twip.py
+++ hello_twip.py
@@ -1,43 +1,13 @@
-import genesis as gs
-import numpy as np
+from twip_env import TwipEnv
+from stable_baselines3 import PPO
 
-gs.init(backend=gs.cuda)
+env = TwipEnv()
+episodes = 500
 
-scene = gs.Scene(
-    show_viewer=True,
-)
-
-plane = scene.add_entity(
-    gs.morphs.Plane(),
-)
-twip = scene.add_entity(
-    gs.morphs.URDF(
-        file="assets/twip.urdf",
-        pos=[0, 0, 0.1]
-    )
-)
-
-joints_name = [
-    "rwheel",
-    "lwheel",
-]
-
-motors_dof_idx = [twip.get_joint(name).dof_idx_local for name in joints_name]
-
-scene.build()
-flip = True
-
-for i in range(1000):
-    if i % 50 == 0:
-        flip = not flip
-
-    if flip:
-        velocity = np.array([10.0, 10.0])
-    else:
-        velocity = np.array([-10.0, -10.0])
-
-    twip.control_dofs_velocity(
-        velocity,
-        motors_dof_idx,
-    )
-    scene.step()
+model = PPO('MlpPolicy', env, device="cpu")
+for episode in range(episodes):
+    done = False
+    obs, _ = env.reset()
+    while not done:
+        action, _states = model.predict(obs)
+        obs, rew, dones, truncated, info = env.step(action)
--- twip_env.py
+++ twip_env.py
@@ -1,80 +1,128 @@
+import math
 import gymnasium as gym
 import numpy as np
+import torch
 import genesis as gs
 from gymnasium import spaces
 
-
 class TwipEnv(gym.Env):
     def __init__(self):
-        super.__init__()
+        super(TwipEnv, self).__init__()
 
-        self.max_torque = 10.0
+        self.device = torch.device("cpu")  # CUDA support
 
-        # define the spaces
-        # Observations are as follows: Pitch angle, Pitch angular velocity, left wheel velocity, right wheel velocity
+        self._max_torque = 10.0
+        self._max_angle = 35.0
+
+        # Define the spaces
+        # Observation space
         self.observation_space = spaces.Box(
             low=np.array([-np.pi, -np.inf, -np.inf, -np.inf], dtype=np.float32),
             high=np.array([np.pi, np.inf, np.inf, np.inf], dtype=np.float32),
             dtype=np.float32
         )
 
-        # Actions are the torques given to the motors
-        self.action_space = spaces.Box(low=np.array([-1.0, -1.0]), high=np.array([1.0, 1.0]), dtype=np.float32)
+        self.obs = torch.zeros((4,), device=self.device)
+        self.rew = torch.tensor(0.0, device=self.device)
+        self.dones = False
+        self.truncated = False
+
+        # Action space
+        self.action_space = spaces.Box(
+            low=np.array([-1.0, -1.0], dtype=np.float32),
+            high=np.array([1.0, 1.0], dtype=np.float32),
+            dtype=np.float32
+        )
+        self.action = torch.zeros((2,), device=self.device)
 
         # Defining the scene
         gs.init(backend=gs.cuda)
-        self.scene = gs.Scene()
-        self.plane = self.scene.add_entity(
-            gs.morphs.Plane(),
-        )
-        self.twip = self.scene.add_entity(
-            gs.morphs.URDF(
-                file="assets/twip.urdf",
-                pos=[0, 0, 0.1]
+        self.scene = gs.Scene(
+            vis_options=gs.options.VisOptions(
+                show_link_frame=True,
             )
         )
-
-        # Defining the dofs
-        self._joints_name = [
-            "rwheel",
-            "lwheel",
-        ]
-        self._motors_dof_idx = [self.twip.get_joint(name).dof_idx_local for name in self._joints_name]
-
-        # Building the Scene
-        self.scene.build()
-
-    def step(self, action):
-        # Apply the actions
-        action = action.copy() * self.max_torque
-
-        self.twip.control_dofs_force(
-            action,
-            self._motors_dof_idx,
+        self.plane = self.scene.add_entity(gs.morphs.Plane())
+        self.twip = self.scene.add_entity(
+            gs.morphs.URDF(file="assets/twip.urdf", pos=[0, 0, 0.1])
         )
 
-        # Step the sim
+        self._joints_name = ["lwheel", "rwheel"]
+        self._motors_dof_idx = [self.twip.get_joint(name).dof_idx_local for name in self._joints_name]
+
+        self.scene.build()
+
+        self._init_pos = self.twip.get_pos()
+        self._init_quat = self.twip.get_quat()
+
+    def step(self, action):
+        self.action = action * self._max_torque
+
+        self.twip.control_dofs_force(self.action, self._motors_dof_idx)
         self.scene.step()
 
-        obs = self._get_obs()
-        rew = self._compute_rew()
-        dones = self._check_dones()
+        self.obs = self._get_obs()
+        self.rew = self._compute_rew()
+        self.dones = self._check_dones()
+        self.truncated = False
 
-        info = {}
+        info = {
+            "position": self.twip.get_pos(),
+            "obs": self.obs,
+            "rew": self.rew,
+        }
 
-        return obs, rew, dones, info
+        return self.obs, self.rew, self.dones, self.truncated, info
 
-    def reset(self):
-        pass
+    def reset(self, seed=None, options=None):
+        self.twip.set_dofs_position(
+            position=torch.tensor([0, 0], dtype=torch.float32, device=self.device),
+            dofs_idx_local=self._motors_dof_idx,
+            zero_velocity=True
+        )
+        self.twip.set_pos(self._init_pos)
+        self.twip.set_quat(self._init_quat)
+
+        self.action = torch.zeros((2,), device=self.device)
+        self.obs = torch.zeros((4,), device=self.device)
+
+        return self.obs, None
 
     def close(self):
         pass
 
     def _get_obs(self):
-        pass
+        curr_pitch = self._get_pitch_ang()
+
+        ang_vel = torch.tensor(self.twip.get_ang(), device=self.device)
+        curr_ang_pitch = ang_vel[2]
+
+        dofs_vel = torch.tensor(self.twip.get_dofs_velocity(self._motors_dof_idx), device=self.device)
+        left_wheel_vel = dofs_vel[0]
+        right_wheel_vel = dofs_vel[1]
+
+        return torch.stack([
+            curr_pitch,
+            curr_ang_pitch,
+            left_wheel_vel,
+            right_wheel_vel
+        ], dim=0)
 
     def _compute_rew(self):
-        pass
+        pitch = self._get_pitch_ang()
+        pitch_threshold = torch.deg2rad(torch.tensor(self._max_angle, device=self.device))
+        reward = 1.0 - (torch.abs(pitch) / pitch_threshold)
+        return torch.clamp(reward, 0.0, 1.0)
 
     def _check_dones(self):
-        pass
+        pitch = self._get_pitch_ang()
+        pitch_threshold = torch.deg2rad(torch.tensor(self._max_angle, device=self.device))
+        return torch.abs(pitch) > pitch_threshold
+
+    def _get_pitch_ang(self):
+        quat = torch.tensor(self.twip.get_quat(), device=self.device)
+        w, x, y, z = quat[0], quat[1], quat[2], quat[3]
+        t0 = 2.0 * (w * y - z * x)
+        t1 = 1.0 - 2.0 * (y * y + x * x)
+        pitch = torch.atan2(t0, t1)
+        return pitch
