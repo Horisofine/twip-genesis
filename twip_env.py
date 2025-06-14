19c173118c4738b904b2c8165624bc0be67f3ef4
Houssam E. Righi <hrighi01@gmail.com>
2025-06-13 22:27:01 -0400

    creating the genesis setup

--- twip_env.py
+++ twip_env.py
@@ -3,17 +3,27 @@ import numpy as np
 import genesis as gs
 from gymnasium import spaces
 
+
 class TwipEnv(gym.Env):
     def __init__(self):
         super.__init__()
 
-        #define the spaces
-        self.observation_space = spaces.Box(low=-35, high=35, shape=(1,), dtype=np.float32)
-        self.action_space = spaces.Box(low=-1, high=1, shape=(2,), dtype=np.float32)
+        self.max_torque = 10.0
 
+        # define the spaces
+        # Observations are as follows: Pitch angle, Pitch angular velocity, left wheel velocity, right wheel velocity
+        self.observation_space = spaces.Box(
+            low=np.array([-np.pi, -np.inf, -np.inf, -np.inf], dtype=np.float32),
+            high=np.array([np.pi, np.inf, np.inf, np.inf], dtype=np.float32),
+            dtype=np.float32
+        )
+
+        # Actions are the torques given to the motors
+        self.action_space = spaces.Box(low=np.array([-1.0, -1.0]), high=np.array([1.0, 1.0]), dtype=np.float32)
+
+        # Defining the scene
         gs.init(backend=gs.cuda)
         self.scene = gs.Scene()
-
         self.plane = self.scene.add_entity(
             gs.morphs.Plane(),
         )
@@ -24,21 +34,47 @@ class TwipEnv(gym.Env):
             )
         )
 
+        # Defining the dofs
         self._joints_name = [
             "rwheel",
             "lwheel",
         ]
         self._motors_dof_idx = [self.twip.get_joint(name).dof_idx_local for name in self._joints_name]
 
+        # Building the Scene
         self.scene.build()
 
     def step(self, action):
+        # Apply the actions
+        action = action.copy() * self.max_torque
 
+        self.twip.control_dofs_force(
+            action,
+            self._motors_dof_idx,
+        )
 
-        pass
+        # Step the sim
+        self.scene.step()
+
+        obs = self._get_obs()
+        rew = self._compute_rew()
+        dones = self._check_dones()
+
+        info = {}
+
+        return obs, rew, dones, info
 
     def reset(self):
         pass
 
     def close(self):
+        pass
+
+    def _get_obs(self):
+        pass
+
+    def _compute_rew(self):
+        pass
+
+    def _check_dones(self):
         pass
