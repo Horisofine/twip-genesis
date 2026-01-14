04681edf9d94d71244b7e0ede7a35e5a3c4f8e06
augus <augustomp55@gmail.com>
2026-01-14 01:28:34 -0500

    Update training parameters and fix environment reset logic in hello_twip.py

--- hello_twip.py
+++ hello_twip.py
@@ -2,10 +2,10 @@ from twip_env import TwipEnv
 from stable_baselines3 import PPO
 
 num_envs = 128
-num_steps_per_env = 500_000
+num_steps_per_env = 8192
 
 env = TwipEnv(num_envs=num_envs)
-model = PPO('MlpPolicy', env, device="cpu", tensorboard_log="./tb", n_steps=128, batch_size=256, n_epochs=5)
+model = PPO('MlpPolicy', env, device="cpu", tensorboard_log="./tb", n_steps=128, batch_size=256, n_epochs=4)
 
 def train():
     try:
@@ -18,13 +18,13 @@ def evaluate():
     obs, _ = env.reset()
     for episode in range(10):
         done = False
-        obs, _ = env.reset()
+        obs = env.reset()
         while not done:
             action, _states = model.predict(obs)
-            obs, rew, dones, truncated, info = env.step(action)
+            obs, rew, dones, info = env.step(action)
             done = dones.any()  # Adjusted for vectorized envs
 
-        print(f"Episode: {episode + 1}, Reward: {rew.sum().item()}")
+            print(f"Episode: {episode + 1}, Reward: {rew.sum().item()}")
 
 if __name__ == "__main__":
     train()
