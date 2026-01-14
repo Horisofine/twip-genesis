d97b1a0a7bf4fc9698540dd87fea603cf491f22a
augus <augustomp55@gmail.com>
2026-01-14 01:23:09 -0500

    Refactor TwipEnv to inherit from VecEnv and support multiple environments

--- twip_env.py
+++ twip_env.py
@@ -1,48 +1,53 @@
 import math
+from typing import Any
+
 import gymnasium as gym
 import numpy as np
 import torch
 import genesis as gs
 from gymnasium import spaces
+from stable_baselines3.common.vec_env import VecEnv
+from stable_baselines3.common.vec_env.base_vec_env import VecEnvIndices
 
 
-class TwipEnv(gym.Env):
-    def __init__(self):
-        super(TwipEnv, self).__init__()
-
-        self.device = torch.device("cpu")  # CUDA support
-
-        self._max_torque = 10.0
-        self._max_yaw = 14.75
-
-        # Define the spaces
-        # Observation space
-        self.observation_space = spaces.Box(
+class TwipEnv(VecEnv):
+    def __init__(self, num_envs: int = 8):
+        observation_space = spaces.Box(
             low=np.array([-np.pi, -np.inf, -np.inf, -np.inf], dtype=np.float32),
             high=np.array([np.pi, np.inf, np.inf, np.inf], dtype=np.float32),
             dtype=np.float32
         )
-
-        self.num_envs = 32
-
-        self.obs = torch.zeros((self.num_envs, 4), device=self.device)
-        self.rew = torch.zeros((self.num_envs,), device=self.device)
-        self.dones = False
-        self.truncated = False
-
-        # Action space
-        self.action_space = spaces.Box(
+        action_space = spaces.Box(
             low=np.array([-1.0, -1.0], dtype=np.float32),
             high=np.array([1.0, 1.0], dtype=np.float32),
             dtype=np.float32
         )
-        self.action = torch.zeros((self.num_envs, 2), device=self.device)
+        super(TwipEnv, self).__init__(
+            num_envs=num_envs,
+            observation_space=observation_space,
+            action_space=action_space,
+        )
+
+        self.render_mode = "human"
+        self.device = torch.device("cpu")  # CUDA support
+
+        self._max_torque = 10.0
+        self._touchdown_yaw = 14.75  # degrees
+        self._max_yaw = 17.0  # degrees
+        self._max_episode_length = 512  # steps
+
+        self.obs = torch.zeros((self.num_envs, 4), device=self.device)
+        self.actions = torch.zeros((self.num_envs, 2), device=self.device)
+        self.rew = torch.zeros((self.num_envs,), device=self.device)
+        self.lengths = torch.zeros((self.num_envs,), device=self.device)
+        self.dones = False
+        self.truncated = False
 
         # Defining the scene
         gs.init(backend=gs.cpu)
         from genesis.engine.entities import RigidEntity
 
-        max_rendered_num_envs = 4
+        max_rendered_num_envs = 3
         self.scene = gs.Scene(
             vis_options=gs.options.VisOptions(
                 show_link_frame=False,
@@ -51,7 +56,8 @@ class TwipEnv(gym.Env):
             viewer_options=gs.options.ViewerOptions(
                 res=(1200, 800),
             ),
-            show_viewer=True
+            show_viewer=True,
+            show_FPS=False,
         )
         self.plane = self.scene.add_entity(gs.morphs.Plane())
         self.twip: RigidEntity = self.scene.add_entity(
@@ -74,42 +80,53 @@ class TwipEnv(gym.Env):
         self._init_pos = self.twip.get_pos().clone()
         self._init_quat = self.twip.get_quat().clone()
 
-    def step(self, action):
-        self.action = action * self._max_torque
+    def step_async(self, actions: np.ndarray) -> None:
+        self.actions = torch.tensor(actions, device=self.device) * self._max_torque
 
-        self.twip.control_dofs_force(self.action, self._motors_dof_idx)
+    def step_wait(self):
+        self.twip.control_dofs_force(self.actions, self._motors_dof_idx)
         self.scene.step()
 
         self.dones = self._check_dones()
-        self.truncated = False
+        self.truncated = self.lengths >= self._max_episode_length
+
+        envs_to_reset = self.dones | self.truncated
 
         # create the env idx tensor for all environments that are done (i.e. are True)
-        done_envs_idx = torch.nonzero(self.dones, as_tuple=False).squeeze(-1)
+        done_envs_idx = torch.nonzero(envs_to_reset, as_tuple=False).squeeze(-1)
 
         self.obs = self._get_obs()
         self.rew = self._compute_rew()
+        self.lengths += 1
 
         reset_obs, reset_action, reset_rew = self._reset_envs(envs_idx=done_envs_idx)
 
         self.obs[done_envs_idx] = reset_obs
-        self.action[done_envs_idx] = reset_action
+        self.actions[done_envs_idx] = reset_action
         self.rew[done_envs_idx] = reset_rew
+        self.lengths[done_envs_idx] = 0
 
-        info = {
-            "position": self.twip.get_pos(),
-            "obs": self.obs,
-            "rew": self.rew,
-        }
+        infos = []
+        for i in range(self.num_envs):
+            infos.append({
+                "position": self.twip.get_pos(i),  # per-env
+                "obs": self.obs[i].cpu().numpy(),
+                "action": self.actions[i].cpu().numpy(),
+                "rew": float(self.rew[i]),
+                "done": bool(self.dones[i]),
+                "truncated": bool(self.truncated[i]),
+                "length": int(self.lengths[i]),
+            })
 
-        return self.obs, self.rew, self.dones, self.truncated, info
+        return self.obs.numpy(), self.rew.numpy(), self.dones.numpy(), infos
 
     def reset(self, seed=None, options=None, envs_idx: torch.Tensor | None = None):
         if envs_idx is None:
             envs_idx = torch.arange(self.num_envs, device=self.device)
 
-        self.obs[envs_idx], self.action[envs_idx], self.rew[envs_idx] = self._reset_envs(envs_idx=envs_idx)
+        self.obs[envs_idx], self.actions[envs_idx], self.rew[envs_idx] = self._reset_envs(envs_idx=envs_idx)
 
-        return self.obs, None
+        return self.obs.numpy()
 
     def close(self):
         pass
@@ -121,6 +138,7 @@ class TwipEnv(gym.Env):
             envs_idx=envs_idx,
             zero_velocity=True
         )
+
         self.twip.set_pos(
             self._init_pos[envs_idx],
             envs_idx=envs_idx,
@@ -157,7 +175,7 @@ class TwipEnv(gym.Env):
 
     def _compute_rew(self):
         yaw, _, _ = self._get_euler_ang()
-        yaw_threshold = torch.deg2rad(torch.tensor(self._max_yaw, device=self.device))
+        yaw_threshold = torch.deg2rad(torch.tensor(self._touchdown_yaw, device=self.device))
         reward = 1.0 - (torch.abs(yaw) / yaw_threshold)
         return torch.clamp(reward, 0.0, 1.0)
 
@@ -182,3 +200,15 @@ class TwipEnv(gym.Env):
         roll = torch.atan2(t3, t4)
 
         return yaw, pitch, roll
+
+    def get_attr(self, attr_name: str, indices: VecEnvIndices = None) -> list[Any]:
+        return [getattr(self, attr_name) for _ in range(self.num_envs)]
+
+    def set_attr(self, attr_name: str, value: Any, indices: VecEnvIndices = None) -> None:
+        setattr(self, attr_name, value)
+
+    def env_method(self, method_name: str, *method_args, indices: VecEnvIndices = None, **method_kwargs) -> list[Any]:
+        return [getattr(self, method_name)(*method_args, **method_kwargs) for _ in range(self.num_envs)]
+
+    def env_is_wrapped(self, wrapper_class: type[gym.Wrapper], indices: VecEnvIndices = None) -> list[bool]:
+        return [isinstance(self, wrapper_class) for _ in range(self.num_envs)]
